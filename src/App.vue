4633bc12f95dfd11924c9bf52ebd8cf5cf8301c1
Cole Pedersen <cole.pedersen@gmail.com>
2019-04-09 15:10:49 -0600

    addtodo comp, with style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <Header/>
+    <AddTodo/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
@@ -7,11 +9,15 @@
 
 <script>
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import Header from "./components/layout/Header";
 
 export default {
   name: "app",
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
   },
   data() {
     return {
@@ -55,5 +61,14 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
 }
 </style>
